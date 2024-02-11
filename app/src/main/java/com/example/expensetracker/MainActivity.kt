fe4d9c061b311bffc469de49b963867123fcfa6b
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-02-11 21:07:57 +0530

    Refactored code to be more elegant

--- app/src/main/java/com/example/expensetracker/MainActivity.kt
+++ app/src/main/java/com/example/expensetracker/MainActivity.kt
@@ -44,7 +44,6 @@ class MainActivity : ComponentActivity() {
                 isUsed = metadata?.infoValue == "TRUE"
             }
         }
-
         return isUsed
     }
 }
--- app/src/main/java/com/example/expensetracker/ui/theme/Color.kt
+++ app/src/main/java/com/example/expensetracker/ui/theme/Color.kt
@@ -1,6 +1,5 @@
 package com.example.expensetracker.ui.theme
 
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.ui.graphics.Color
 
 val Purple80 = Color(0xFFD0BCFF)
