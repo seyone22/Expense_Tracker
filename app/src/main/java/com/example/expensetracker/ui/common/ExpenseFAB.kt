5310cdb1790e17477283480b2071ed7a4f0d5a51
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-03-04 00:25:26 +0530

    BUGFIX: FAB and Navbar showing up where they aren't supposed to

--- app/src/main/java/com/example/expensetracker/ui/common/ExpenseFAB.kt
+++ app/src/main/java/com/example/expensetracker/ui/common/ExpenseFAB.kt
@@ -1,19 +1,26 @@
 package com.example.expensetracker.ui.common
 
-import android.util.Log
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import com.example.expensetracker.ui.screen.accounts.AccountsDestination
+import com.example.expensetracker.ui.screen.entities.EntitiesDestination
+import com.example.expensetracker.ui.screen.report.BudgetsDestination
+import com.example.expensetracker.ui.screen.report.ReportsDestination
+import com.example.expensetracker.ui.screen.transactions.TransactionsDestination
 
 @Composable
 fun ExpenseFAB(
     navigateToScreen: (screen: String) -> Unit,
+    currentActivity: String
 ) {
-    FloatingActionButton(onClick = {
-        navigateToScreen("TransactionEntry")
-    }) {
-        Icon(Icons.Outlined.Edit, "Add")
+    if ((currentActivity == AccountsDestination.route) or (currentActivity == EntitiesDestination.route) or (currentActivity == BudgetsDestination.route) or (currentActivity == TransactionsDestination.route) or (currentActivity == ReportsDestination.route)) {
+        FloatingActionButton(onClick = {
+            navigateToScreen("TransactionEntry")
+        }) {
+            Icon(Icons.Outlined.Edit, "Add")
+        }
     }
 }
