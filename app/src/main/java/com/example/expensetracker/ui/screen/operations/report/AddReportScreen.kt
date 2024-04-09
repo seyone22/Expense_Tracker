8057c7e08d5fcc2598ef4906c26417a10b7b8aa5
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-04-09 16:58:04 +0530

    Report adding screen added, does not work yet

---
+++ app/src/main/java/com/example/expensetracker/ui/screen/operations/report/AddReportScreen.kt
@@ -0,0 +1,103 @@
+package com.example.expensetracker.ui.screen.operations.report
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.expensetracker.R
+import com.example.expensetracker.ui.AppViewModelProvider
+import com.example.expensetracker.ui.navigation.NavigationDestination
+import com.example.expensetracker.ui.screen.operations.transaction.TransactionEntryViewModel
+import kotlinx.coroutines.launch
+
+object ReportEntryDestination : NavigationDestination {
+    override val route = "ReportEntry"
+    override val titleRes = R.string.app_name
+    override val routeId = 12
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ReportEntryScreen(
+    modifier: Modifier = Modifier,
+    navigateBack: () -> Unit = {},
+    onNavigateUp: () -> Unit = {},
+    canNavigateBack: Boolean = true,
+    viewModel: TransactionEntryViewModel = viewModel(factory = AppViewModelProvider.Factory),
+) {
+    val coroutineScope = rememberCoroutineScope()
+
+    Scaffold(
+        containerColor = MaterialTheme.colorScheme.background,
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.background,
+                    titleContentColor = MaterialTheme.colorScheme.onSurface,
+                ),
+                title = {
+                    Text(
+                        text = "Create Report",
+                        style = MaterialTheme.typography.titleLarge
+                    )
+                },
+                navigationIcon = {
+                    IconButton(onClick = {
+                        navigateBack()
+                    }) {
+                        Icon(
+                            imageVector = Icons.Filled.Close,
+                            contentDescription = "Close"
+                        )
+                    }
+                },
+                actions = {
+                    Button(
+                        onClick = {
+                            coroutineScope.launch {
+                                viewModel.saveRecurringTransaction()
+                                navigateBack()
+                            }
+                        },
+                        enabled = viewModel.transactionUiState.isEntryValid,
+                        modifier = modifier.padding(0.dp, 0.dp, 8.dp, 0.dp)
+                    ) {
+                        Text(text = "Create")
+                    }
+                }
+            )
+
+        }
+
+    ) { padding ->
+        LazyColumn(
+        ) {
+            item {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(padding),
+                ) {
+
+                }
+            }
+        }
+    }
+}
