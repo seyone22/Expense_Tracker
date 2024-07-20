0246c9b30bf072835f8e57baa659b9834371bd03
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-07-21 01:15:43 +0530

    Migrated database to v2, and added currency history

--- app/src/main/java/com/example/expensetracker/data/MMEXDatabase.kt
+++ app/src/main/java/com/example/expensetracker/data/MMEXDatabase.kt
@@ -4,10 +4,13 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.expensetracker.data.model.Account
 import com.example.expensetracker.data.model.BillsDeposits
 import com.example.expensetracker.data.model.Category
 import com.example.expensetracker.data.model.CurrencyFormat
+import com.example.expensetracker.data.model.CurrencyHistory
 import com.example.expensetracker.data.model.Metadata
 import com.example.expensetracker.data.model.Payee
 import com.example.expensetracker.data.model.Report
@@ -16,14 +19,15 @@ import com.example.expensetracker.data.repository.account.AccountDao
 import com.example.expensetracker.data.repository.billsDeposit.BillsDepositsDao
 import com.example.expensetracker.data.repository.category.CategoryDao
 import com.example.expensetracker.data.repository.currencyFormat.CurrencyFormatDao
+import com.example.expensetracker.data.repository.currencyHistory.CurrencyHistoryDao
 import com.example.expensetracker.data.repository.metadata.MetadataDao
 import com.example.expensetracker.data.repository.payee.PayeeDao
 import com.example.expensetracker.data.repository.report.ReportDao
 import com.example.expensetracker.data.repository.transaction.TransactionDao
 
 @Database(
-    entities = [Account::class, Transaction::class, Payee::class, Category::class, CurrencyFormat::class, Metadata::class, BillsDeposits::class, Report::class],
-    version = 1,
+    entities = [Account::class, Transaction::class, Payee::class, Category::class, CurrencyFormat::class, Metadata::class, BillsDeposits::class, Report::class, CurrencyHistory::class],
+    version = 2,
     exportSchema = true
 )
 abstract class MMEXDatabase : RoomDatabase() {
@@ -35,6 +39,7 @@ abstract class MMEXDatabase : RoomDatabase() {
     abstract fun metadataDao(): MetadataDao
     abstract fun billsDepositsDao(): BillsDepositsDao
     abstract fun reportDao(): ReportDao
+    abstract fun currencyHistoryDao(): CurrencyHistoryDao
 
     companion object {
         @Volatile
@@ -44,9 +49,25 @@ abstract class MMEXDatabase : RoomDatabase() {
             return Instance ?: synchronized(this) {
                 Room.databaseBuilder(context, MMEXDatabase::class.java, "mmex_database")
                     //.createFromAsset("database/prepopulate_v1_1.db")
-                    .fallbackToDestructiveMigration()
+                    .addMigrations(MIGRATION_1_2)
                     .build()
                     .also { Instance = it }
+            }
+        }
+
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL(
+                    """
+            CREATE TABLE IF NOT EXISTS CURRENCYHISTORY_V1 (
+                currHistId INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+                currencyId INTEGER NOT NULL,
+                currDate INTEGER NOT NULL,
+                currValue REAL NOT NULL,
+                currUpdType INTEGER NOT NULL
+            )
+            """.trimIndent()
+                )
             }
         }
     }
