87d977c3c74b24fc95d9c7e3604a9e45c5f9b6f7
Seyone Gunasingham <s.g.seyone@protonmail.com>
2024-07-21 01:35:13 +0530

    Changed the date field to a string

--- app/src/main/java/com/example/expensetracker/data/model/CurrencyHistory.kt
+++ app/src/main/java/com/example/expensetracker/data/model/CurrencyHistory.kt
@@ -8,7 +8,7 @@ data class CurrencyHistory(
     @PrimaryKey(autoGenerate = true)
     val currHistId: Int = 0, //make this into an autonumber of some sort.
     val currencyId: Int,
-    val currDate: Int,
+    val currDate: String,
     val currValue: Double,
     val currUpdType: Int
 )
