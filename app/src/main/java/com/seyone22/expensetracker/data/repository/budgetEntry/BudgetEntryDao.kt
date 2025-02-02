c5ee172a802dbf9597d8c9f005bc8b02d48f7116
Seyone Gunasingham <s.g.seyone@protonmail.com>
2025-02-03 03:23:03 +0530

    feat: Add BudgetEntry repository and DAO
    - Added `BudgetEntryRepository` interface for budget entry data access.
    - Implemented `OfflineBudgetEntryRepository` to interact with the database.
    - Created `BudgetEntryDao` to define database operations for budget entries.
    - Updated `AppContainer` to include `budgetEntryRepository`.

--- app/src/main/java/com/seyone22/expensetracker/data/repository/budgetEntry/BudgetEntryDao.kt
+++ app/src/main/java/com/seyone22/expensetracker/data/repository/budgetEntry/BudgetEntryDao.kt
@@ -6,18 +6,27 @@ import androidx.room.Insert
 import androidx.room.Query
 import androidx.room.Update
 import com.seyone22.expensetracker.data.model.BudgetEntry
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface BudgetEntryDao {
     @Insert
-    suspend fun insertBudgetEntry(entry: BudgetEntry)
+    suspend fun insert(entry: BudgetEntry)
 
     @Update
-    suspend fun updateBudgetEntry(entry: BudgetEntry)
+    suspend fun update(entry: BudgetEntry)
 
     @Delete
-    suspend fun deleteBudgetEntry(entry: BudgetEntry)
+    suspend fun delete(entry: BudgetEntry)
 
     @Query("SELECT * FROM BUDGETTABLE_V1 WHERE active = 1")
-    suspend fun getActiveBudgetEntries(): List<BudgetEntry>
+    fun getActiveBudgetEntries(): Flow<List<BudgetEntry>>
+
+    @Query("SELECT * FROM BUDGETTABLE_V1 WHERE active = 1 AND budgetEntryId = :budgetYearId")
+    fun getActiveBudgetEntriesForBudgetYearId(budgetYearId: Int): Flow<List<BudgetEntry>>
+
+    @Query("SELECT * FROM BUDGETTABLE_V1 WHERE active = 1 AND budgetEntryId = :budgetEntryId")
+    fun getBudgetEntryById(budgetEntryId: Int): Flow<BudgetEntry>
+
+
 }
