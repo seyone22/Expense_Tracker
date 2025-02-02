39e3a6c61bfdb18d952538b751d1fc5936f552d4
Seyone Gunasingham <s.g.seyone@protonmail.com>
2025-02-02 23:13:38 +0530

    Refactor BudgetScreen and BudgetViewModel
    - Added `BudgetItemCard` composable to display budget information.
    - Implemented `BudgetUiState` to manage the state of categories.
    - Updated `BudgetViewModel` to include category data and combined data from parent and sub-category flows.
    - Added `ExpenseTopBar` for navigation and titles.
    - Displayed parent categories in a lazy list in `BudgetScreen`.

--- app/src/main/java/com/seyone22/expensetracker/ui/screen/budget/BudgetViewModel.kt
+++ app/src/main/java/com/seyone22/expensetracker/ui/screen/budget/BudgetViewModel.kt
@@ -1,17 +1,40 @@
 package com.seyone22.expensetracker.ui.screen.budget
 
 import androidx.lifecycle.ViewModel
+import com.seyone22.expensetracker.data.model.Category
+import com.seyone22.expensetracker.data.repository.category.CategoriesRepository
 import com.seyone22.expensetracker.data.repository.payee.PayeesRepository
+import com.seyone22.expensetracker.data.repository.transaction.TransactionsRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.combine
 
 /**
  * ViewModel to retrieve all items in the Room database.
  */
 class BudgetViewModel(
-
+    private val transactionsRepository: TransactionsRepository,
+    private val categoriesRepository: CategoriesRepository,
     private val payeesRepository: PayeesRepository,
-
-    ) : ViewModel() {
+) : ViewModel() {
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
     }
+
+    private val categoriesParentFlow: Flow<List<Category>> =
+        categoriesRepository.getAllParentCategories()
+    private val categoriesSubFlow: Flow<List<Category>> = categoriesRepository.getAllSubCategories()
+
+    // Combine the flows
+    val budgetUiState: Flow<BudgetUiState> = combine(
+        categoriesParentFlow,
+        categoriesSubFlow,
+    ) { categoriesParent, categoriesSub ->
+        BudgetUiState(categoriesParent, categoriesSub)
+    }
 }
+
+//Data class for BudgetUiState
+data class BudgetUiState(
+    val categoriesParent: List<Category> = listOf(),
+    val categoriesSub: List<Category> = listOf(),
+)
