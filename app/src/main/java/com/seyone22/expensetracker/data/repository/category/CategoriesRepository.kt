705f95bf79e16b365ac432c58cb050ca1837cada
Seyone Gunasingham <s.g.seyone@protonmail.com>
2025-02-02 23:14:56 +0530

    Reorder CategoriesRepository methods.

--- app/src/main/java/com/seyone22/expensetracker/data/repository/category/CategoriesRepository.kt
+++ app/src/main/java/com/seyone22/expensetracker/data/repository/category/CategoriesRepository.kt
@@ -8,13 +8,11 @@ interface CategoriesRepository {
     fun getAllCategoriesStream(): Flow<List<Category>>
     fun getAllActiveCategoriesStream(): Flow<List<Category>>
 
-    suspend fun insertCategory(category: Category)
-    suspend fun deleteCategory(category: Category)
-    suspend fun updateCategory(category: Category)
-
     fun getAllParentCategories(): Flow<List<Category>>
     fun getAllSubCategories(): Flow<List<Category>>
 
-
+    suspend fun insertCategory(category: Category)
     suspend fun insertIfNotExists(category: Category)
+    suspend fun deleteCategory(category: Category)
+    suspend fun updateCategory(category: Category)
 }
