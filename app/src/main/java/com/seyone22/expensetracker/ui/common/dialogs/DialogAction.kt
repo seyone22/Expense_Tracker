d800bc1393b96c7f8c4e59370265222e243d8e16
Seyone Gunasingham <s.g.seyone@protonmail.com>
2025-02-05 01:06:41 +0530

    Refactor DialogAction interface to allow nullable message
    - Updated the `DialogAction` interface to allow a nullable `message` property.
    - Modified the `message` property in `DialogAction` to be of type `String?`, allowing it to be null.

--- app/src/main/java/com/seyone22/expensetracker/ui/common/dialogs/DialogAction.kt
+++ app/src/main/java/com/seyone22/expensetracker/ui/common/dialogs/DialogAction.kt
@@ -4,7 +4,7 @@ import androidx.compose.runtime.Composable
 
 interface DialogAction {
     val title: String
-    val message: String
+    val message: String?
     val content: @Composable () -> Unit // Content for the body of the dialog
     fun onConfirm() // Action when user confirms
     fun onCancel() // Action when user cancels
