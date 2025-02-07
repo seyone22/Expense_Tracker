f7b5075fe1999945982bb0702fb6ba137a3c001d
Seyone Gunasingham <s.g.seyone@protonmail.com>
2025-02-08 01:17:31 +0530

    feat: Refactor QuickActions composable for improved functionality and styling
    - Changed `QuickActions` to use a data class `QuickAction` for action details.
    - Added color to `QuickAction` to show actions in different backgrounds.
    - Updated `QuickActions` to support navigation based on the `destination` property.
    - Modified the icons and content descriptions to use the properties from the `QuickAction` data class.
    - Updated `QuickActions` to allow only `Deposit`, `Withdraw`, `Transfer`, and `Account` to navigate to the target destination.
    - Updated `QuickActions` to utilize `MaterialTheme.colorScheme` for consistent styling.

--- app/src/main/java/com/seyone22/expensetracker/ui/screen/home/composables/QuickActions.kt
+++ app/src/main/java/com/seyone22/expensetracker/ui/screen/home/composables/QuickActions.kt
@@ -1,5 +1,6 @@
 package com.seyone22.expensetracker.ui.screen.home.composables
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -19,17 +20,28 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Tag
 import androidx.compose.material3.Card
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.seyone22.expensetracker.ui.navigation.NavigationDestination
 import com.seyone22.expensetracker.ui.screen.home.HomeViewModel
 import com.seyone22.expensetracker.ui.screen.operations.account.AccountEntryDestination
 import com.seyone22.expensetracker.ui.screen.operations.transaction.TransactionEntryDestination
 import kotlinx.coroutines.CoroutineScope
+
+data class QuickAction(
+    val title: String,
+    val icon: ImageVector,
+    val destination: NavigationDestination,
+    val color: Color
+)
 
 @Composable
 fun QuickActions(
@@ -39,16 +51,54 @@ fun QuickActions(
     coroutineScope: CoroutineScope = rememberCoroutineScope()
 ) {
     val actions = listOf(
-        Triple("Deposit", Icons.Filled.ArrowDownward, TransactionEntryDestination),
-        Triple("Withdraw", Icons.Filled.ArrowUpward, TransactionEntryDestination),
-        Triple(
-            "Transfer", Icons.AutoMirrored.Filled.CompareArrows, TransactionEntryDestination
+        QuickAction(
+            "Deposit",
+            Icons.Filled.ArrowDownward,
+            TransactionEntryDestination,
+            MaterialTheme.colorScheme.primaryContainer
         ),
-        Triple("Account", Icons.Filled.AccountBalanceWallet, AccountEntryDestination),
-        Triple("Payee", Icons.Filled.Person, TransactionEntryDestination),
-        Triple("Category", Icons.Filled.Bookmark, TransactionEntryDestination),
-        Triple("Tag", Icons.Filled.Tag, TransactionEntryDestination),
-        Triple("Currency", Icons.Filled.CurrencyYen, TransactionEntryDestination)
+        QuickAction(
+            "Withdraw",
+            Icons.Filled.ArrowUpward,
+            TransactionEntryDestination,
+            MaterialTheme.colorScheme.primaryContainer
+        ),
+        QuickAction(
+            "Transfer",
+            Icons.AutoMirrored.Filled.CompareArrows,
+            TransactionEntryDestination,
+            MaterialTheme.colorScheme.primaryContainer
+        ),
+        QuickAction(
+            "Account",
+            Icons.Filled.AccountBalanceWallet,
+            AccountEntryDestination,
+            MaterialTheme.colorScheme.tertiaryContainer
+        ),
+        QuickAction(
+            "Payee",
+            Icons.Filled.Person,
+            TransactionEntryDestination,
+            MaterialTheme.colorScheme.inversePrimary
+        ),
+        QuickAction(
+            "Category",
+            Icons.Filled.Bookmark,
+            TransactionEntryDestination,
+            MaterialTheme.colorScheme.inversePrimary
+        ),
+        QuickAction(
+            "Tag",
+            Icons.Filled.Tag,
+            TransactionEntryDestination,
+            MaterialTheme.colorScheme.inversePrimary
+        ),
+        QuickAction(
+            "Currency",
+            Icons.Filled.CurrencyYen,
+            TransactionEntryDestination,
+            MaterialTheme.colorScheme.inversePrimary
+        )
     )
 
     LazyRow(
@@ -59,8 +109,8 @@ fun QuickActions(
             Card(modifier = Modifier
                 .size(100.dp)
                 .clickable {
-                    if (actions[index].first == "Deposit" || actions[index].first == "Withdraw" || actions[index].first == "Transfer" || actions[index].first == "Account") {
-                        navigateToScreen(actions[index].third.route)
+                    if (actions[index].title == "Deposit" || actions[index].title == "Withdraw" || actions[index].title == "Transfer" || actions[index].title == "Account") {
+                        navigateToScreen(actions[index].destination.route)
                     } else {
                         /*                        viewModel.showDialog(
                                                     AddEditBudgetYearDialogAction(
@@ -77,19 +127,21 @@ fun QuickActions(
                 }) {
                 Box(
                     contentAlignment = Alignment.Center, // Centers content inside the Box
-                    modifier = Modifier.fillMaxSize() // Ensures the Box takes up the full Card size
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(actions[index].color) // Ensures the Box takes up the full Card size
                 ) {
                     Column(
                         horizontalAlignment = Alignment.CenterHorizontally, // Centers content horizontally
                         verticalArrangement = Arrangement.Center // Centers content vertically
                     ) {
                         Icon(
-                            actions[index].second,
-                            contentDescription = actions[index].first,
+                            actions[index].icon,
+                            contentDescription = actions[index].title,
                             modifier = Modifier.size(36.dp)
                         )
                         Text(
-                            text = actions[index].first,
+                            text = actions[index].title,
                             modifier = Modifier.padding(8.dp),
                             textAlign = TextAlign.Center
                         )
