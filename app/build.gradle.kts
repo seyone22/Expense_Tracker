864b7899b8167b3819d64f4fd9d2dbf989e6a12a
Seyone Gunasingham <s.g.seyone@protonmail.com>
2025-03-10 08:23:42 +0530

    feat: Bump versionCode to 5 and versionName to 0.4.1-alpha
    - Updated the `versionCode` in `build.gradle.kts` from 4 to 5.
    - Updated the `versionName` in `build.gradle.kts` from 0.4.0 to 0.4.1-alpha.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.seyone22.expenses"
         minSdk = 28
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4.0"
+        versionCode = 5
+        versionName = "0.4.1-alpha"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
