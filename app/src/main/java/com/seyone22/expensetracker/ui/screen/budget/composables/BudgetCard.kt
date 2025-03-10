062801d8a77e2b46abce9ecc19df7d31304e54e9
Seyone Gunasingham <s.g.seyone@protonmail.com>
2025-03-10 08:24:16 +0530

    Refactor: Enhance Budget Detail Screen and add budget composables
    - Moved logic for rendering Budget items to their own composables.
    - Added a `BudgetCard` composable.
    - Enhanced the `BudgetDetailScreen` for a better user experience.
    - Added a Income Expense Summary to budget details.
    - Added dynamic details of the category based on its budget.
    - Removed redundant `UnsetBudgetItemCard` and `BudgetItemCard` Composables from main screen file.
    - Added a new composable `BudgetCategoryItem`.
    - Added an action to show only active budget entries.
    - Fetch Budget statistics in `BudgetDetailViewModel`.
    - Added functions to get Estimated expenses and incomes.
    - Refactor and move several methods for better code organisation.

---
+++ app/src/main/java/com/seyone22/expensetracker/ui/screen/budget/composables/BudgetCard.kt
@@ -0,0 +1,53 @@
+package com.seyone22.expensetracker.ui.screen.budget.composables
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ChevronRight
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.seyone22.expensetracker.data.model.BudgetYear
+import com.seyone22.expensetracker.ui.screen.budget.budgetDetail.BudgetDetailDestination
+
+@Composable
+fun BudgetCard(
+    budgetYear: BudgetYear, isYearBudget: Boolean, navigateToScreen: (screen: String) -> Unit
+) {
+    ListItem(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(64.dp)
+            .clickable { navigateToScreen("${BudgetDetailDestination.route}/${budgetYear.budgetYearId}") },
+        headlineContent = {
+            Row {
+                if (!isYearBudget) {
+                    Spacer(modifier = Modifier.width(16.dp))
+                }
+                Text(
+                    fontWeight = if (isYearBudget) {
+                        FontWeight.Bold
+                    } else {
+                        FontWeight.Normal
+                    }, text = budgetYear.budgetYearName
+                )
+            }
+        },
+        trailingContent = {
+            Icon(
+                Icons.Filled.ChevronRight, "", modifier = Modifier.size(24.dp)
+            )
+        }
+    )
+    HorizontalDivider()
+}
