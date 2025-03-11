f7bf666a0fbc2d10c2ada619c9e39af1dc493119
Seyone Gunasingham <s.g.seyone@protonmail.com>
2025-03-12 00:19:39 +0530

    Bump kotlin serialization plugin to 1.9.22
    - Updated the Kotlin serialization plugin version from 1.9.21 to 1.9.22.
    - Updated `androidx.activity:activity-ktx` dependency declaration to use the `libs` version catalog.
    - Removed unused `androidx.lifecycle:lifecycle-runtime-ktx` dependency.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
 
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.21"
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.22"
 }
 
 android {
@@ -108,7 +108,6 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.runtime.compose)
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
 
     // Retrofit
     implementation(libs.retrofit)
@@ -145,5 +144,5 @@ dependencies {
     // AndroidX Compose Material3 Window Size Class Android
     implementation("androidx.compose.material3:material3-window-size-class-android:1.3.1")
 
-    implementation("androidx.activity:activity-ktx:1.10.0") // Latest version
+    implementation(libs.androidx.activity.ktx) // Latest version
 }
