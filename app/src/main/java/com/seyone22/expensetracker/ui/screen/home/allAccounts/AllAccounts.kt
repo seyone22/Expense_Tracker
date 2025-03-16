e5aa53e407d92042d7313c8ba88287f4db64a0d1
Seyone Gunasingham <s.g.seyone@protonmail.com>
2025-03-17 01:04:34 +0530

    feat: Add AllAccounts screen and update navigation
    - Added `AllAccounts.kt` to display all accounts.
    - Created `AllAccountsDestination` for navigation to the All Accounts screen.
    - Updated `AccountData` to navigate to the new All Accounts screen when the "All Accounts" button is clicked.

---
+++ app/src/main/java/com/seyone22/expensetracker/ui/screen/home/allAccounts/AllAccounts.kt
@@ -0,0 +1,25 @@
+package com.seyone22.expensetracker.ui.screen.home.allAccounts
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.seyone22.expensetracker.R
+import com.seyone22.expensetracker.ui.AppViewModelProvider
+import com.seyone22.expensetracker.ui.navigation.NavigationDestination
+import com.seyone22.expensetracker.ui.screen.home.HomeViewModel
+
+object AllAccountsDestination : NavigationDestination {
+    override val route = "All Accounts"
+    override val titleRes = R.string.app_name
+    override val routeId = 32
+}
+
+@Composable
+fun AllAccounts(
+    modifier: Modifier = Modifier,
+    navigateBack: () -> Unit,
+    navigateToScreen: (screen: String) -> Unit,
+    viewModel: HomeViewModel = viewModel(factory = AppViewModelProvider.Factory)
+) {
+
+}
